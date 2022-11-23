b1cee4ac12d63f9362d6f049c6fc66c40bc2f0fc
Windows <118860604+GucciExploiter@users.noreply.github.com>
2022-11-23 10:13:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ clear(clean)
 
 try:
     from requests import get
-except:
-    system(executable+' python -m pip install --upgrade pip && pip3 install requests')
-    import requests
+except Exception:
+    try:
+        system(executable+' python -m pip install --upgrade pip && pip3 install requests')
+        import requests
+    except:
+        print('[%s!%s] Não foi possivel instalar os modulos faça isso manualmente [ python -m pip install --upgrade pip && pip3 install requests ]')
 try:
     print('[%s+%s] Olá bem vindo ao Painel.\n[%s+%s] Antes de continuar o painel ainda esta incompleto ;-;\n[%s+%s] pretendo atualizar ele quando tiver tempo\n[%s+%s] Se vc tiver alguma api manda ai po\n[%s-%s]Discord: Gucci#2661'%(G,C,G,C,G,C,G,C,Y,C))
     input('\n%s> %s| %sAperte enter para continuar %s|%s <%s'%(G,C,B,C,G,C))
