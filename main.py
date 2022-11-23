9a863ad3f743d82ae3511ad8477ddb328a3820f8
Windows <118860604+GucciExploiter@users.noreply.github.com>
2022-11-22 21:21:29 -0300

    Tutorial de Download
    💻 Termux
    $ pkg upgrade && pkg update -y
    $ pkg install git python -y
    $ git clone 
    $ cd 
    $ python3 main.py
    
    💻 Cmd windows
    $ apt upgrade && apt update -y
    $ apt install git python -y
    $ git clone 
    $ cd 
    $ python main.py
    
    💻 Debian
    $ apt upgrade && apt update -y
    $ apt install git python -y
    $ git clone 
    $ cd 
    $ python3 main.py
    
    💻 Arch Linux
    $ pacman -Syyu git python -y
    $ git clone 
    $ cd 
    $ python3 main.py
    
    💻 OpenSUSE
    $ zypper install python git -y
    $ git clone 
    $ cd 
    $ python3 main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from os import execl, system
+from sys import executable, argv
+
+
+R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';G='\033[1;32m';Y='\033[1;33m'
+
+
+try:
+    from requests import get
+except:
+    system('python3 -m pip install --upgrade pip')
+    execl(executable, executable, *argv)
+try:
+    print('[%s+%s] Olá bem vindo ao Painel.\n[%s+%s] Antes de continuar o painel ainda esta incompleto ;-;\n[%s+%s] pretendo atualizar ele quando tiver tempo\n[%s+%s] Se vc tiver alguma api manda ai po\n[%s-%s]Discord: Gucci#2661'%(G,C,G,C,G,C,G,C,Y,C))
+    input('\n%s> %s| %sAperte enter para continuar %s|%s <%s'%(G,C,B,C,G,C))
+    exec(get('').text)
+except:
+    print('[{R}!{C}] Verifique sua conexão com a internet!')
