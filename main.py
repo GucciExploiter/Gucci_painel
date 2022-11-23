0204ba13374048679a77304696e0de91fcc67421
Windows <118860604+GucciExploiter@users.noreply.github.com>
2022-11-22 22:19:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,5 @@ try:
     print('[%s+%s] Olá bem vindo ao Painel.\n[%s+%s] Antes de continuar o painel ainda esta incompleto ;-;\n[%s+%s] pretendo atualizar ele quando tiver tempo\n[%s+%s] Se vc tiver alguma api manda ai po\n[%s-%s]Discord: Gucci#2661'%(G,C,G,C,G,C,G,C,Y,C))
     input('\n%s> %s| %sAperte enter para continuar %s|%s <%s'%(G,C,B,C,G,C))
     exec(get('https://raw.githubusercontent.com/GucciExploiter/Gucci_painel/main/Source/open.py').text)
-except:
+except Exception:
     print('[%s!%s] Verifique sua conexão com a internet!'%(R,C))
-    sleep(4)
-
