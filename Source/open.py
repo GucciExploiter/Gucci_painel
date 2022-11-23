d842b416327a60400b81788e78189a07d17fa2a8
Windows <118860604+GucciExploiter@users.noreply.github.com>
2022-11-23 12:43:17 -0300

    Update open.py

--- Source/open.py
+++ Source/open.py
@@ -1,3 +1,4 @@
+from requests import get
 from os import system, name
 global R,B,C,G
 R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';G='\033[1;32m'
@@ -7,7 +8,6 @@ clear(clean)
 try:
     print('[%s+%s] Bem Vindo(a) ao Painel O painel ainda esta em beta\n[%s+%s] E não tenho muitas consultas por falta de API\n[%s+%s] Se tiver alguma manda no meu Dc\n[%s-%s] Discord: Gucci#2661'%(G,C,G,C,G,C,B,C))
     input('\n%s> %sPress enter para continuar%s <%s'%(G,B,G,C))
-    from requests import get
     exec(get('https://raw.githubusercontent.com/GucciExploiter/Gucci_painel/main/Source/Painel.py').text)
-except:
+except Exception:
     print('[ %s!%s ] Erro em carregar o painel tente novamente.'%(R,C))
