75f793b647b89210b07a1d9bcb878bb7a31de9e5
Windows <118860604+GucciExploiter@users.noreply.github.com>
2022-11-23 10:01:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ clear(clean)
 try:
     from requests import get
 except:
-    system(executable+' python3 -m pip install --upgrade pip && pip3 install requests')
+    system(executable+' python -m pip install --upgrade pip && pip3 install requests')
     import requests
 try:
     print('[%s+%s] Olá bem vindo ao Painel.\n[%s+%s] Antes de continuar o painel ainda esta incompleto ;-;\n[%s+%s] pretendo atualizar ele quando tiver tempo\n[%s+%s] Se vc tiver alguma api manda ai po\n[%s-%s]Discord: Gucci#2661'%(G,C,G,C,G,C,G,C,Y,C))
