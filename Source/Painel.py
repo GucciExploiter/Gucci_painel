c9f9f4763db5bfb62285c5bbddd62d3dbbe64928
Windows <118860604+GucciExploiter@users.noreply.github.com>
2022-11-24 13:28:00 -0300

    Update Painel.py

--- Source/Painel.py
+++ Source/Painel.py
@@ -6,7 +6,7 @@ from os import system,name
 global R,B,C,G
 R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';G='\033[1;32m';Y='\033[1;33m'
 
-lista_ips = ['192.141.196.7','45.238.43.40','','','']
+lista_ips = ['192.141.196.1','45.238.43.40','','','']
 
 # pegando o ip
 try:
@@ -27,10 +27,17 @@ except:
 	print('%s[%s ! %s] Verifique sua conexão à Internet! \n%s'%(C,R,C))
 	exit()
 
-for ip in lista_ips:
-    if ipmenu == ip:
-        print('Bem-vindo(a)')
-        break
+def test():
+    for ip in lista_ips:
+        if ipmenu == ip:
+            print('[%s+%s] Bem-vindo(a) %sVIP%s [%s%s%s]'%(G,C,B,G,Y,ip,C))
+            sleep(3)
+            menu()
+        if ipmenu != ip:
+            print('[%s!%s] Voce tem um token valido porem seu ip n esta na WhiteList :C'%(R,C))
+            print('\n[%s-%s] Discord : %sGucci#2661%s | Mande seu ip para usar o painel'%(Y,C,B,C))
+            sleep(3)
+            exit()
 
 
 # versao do painel
@@ -208,4 +215,4 @@ def menu():
 if __name__ == '__main__':
 	global clean
 	clean ={'nt':'cls','posix':'clear'}[name]
-	menu()
+	test()
