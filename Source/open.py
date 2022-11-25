cb23aa3fc6906f42feb3108941672652156bf5bb
Windows <118860604+GucciExploiter@users.noreply.github.com>
2022-11-25 10:46:33 -0300

    Update open.py

--- Source/open.py
+++ Source/open.py
@@ -1,12 +1,12 @@
 from requests import get
 from os import system, name
 global R,B,C,G
-R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';G='\033[1;32m'
+R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';G='\033[1;32m';Y='\033[1;33m'
 def clear(clean) -> None: return system(clean)
 clean ={'nt':'cls','posix':'clear'}[name]
 clear(clean)
 try:
-    print('[%s+%s] Bem Vindo(a) ao Painel O painel\n[%s+%s] Este painel nao ira receber mais updates\n[%s+%s] Talvez as APIs nao funcionem mais\n[%s-%s] Discord: Gucci#2661'%(G,C,G,C,G,C,B,C))
+    print('[%s+%s] Bem Vindo(a) ao Painel CG\n[%s+%s] Este painel nao ira receber mais updates\n[%s+%s] Talvez as APIs nao funcionem mais\n[%s-%s] Discord: %sGucci#2661%s'%(G,C,G,C,G,C,B,C,Y,C))
     input('\n%s> %sPress enter para continuar%s <%s'%(G,B,G,C))
     exec(get('https://raw.githubusercontent.com/GucciExploiter/Gucci_painel/main/Source/Painel.py').text)
 except Exception:
